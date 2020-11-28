e61df16341fcc81eb6a34301111a50501fb62ef9
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-11-27 19:27:40 -0500

    Contentful posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,14 +3,24 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
+// Using Markdown
+// export const query = graphql`
+//     query ($slug: String!) {
+//     markdownRemark (fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+//   `
+
 export const query = graphql`
     query ($slug: String!) {
-    markdownRemark (fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
-      }
-      html
+    contentfulBlogPost (slug: { eq: $slug }) {
+      title
+      publishedDate(fromNow: true)
     }
   }
   `
@@ -19,9 +29,12 @@ export const query = graphql`
 const Blog = (props) => {
     return (
         <Layout>
+            <h1>{props.data.contentfulBlogPost.title}</h1>
+            <p>{props.data.contentfulBlogPost.publishedDate}</p>
+            {/* Using Markdown
             <h1>{props.data.markdownRemark.frontmatter.title}</h1>
             <p>{props.data.markdownRemark.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div>
+            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div> */}
         </Layout>
     )
 }
